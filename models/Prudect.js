703e27bf1a2f2250e590d6d2bee17ab892717c30
Omardh10 <omardaher232@gmail.com>
2025-08-27 11:23:09 +0300

    add part of code

--- models/Prudect.js
+++ models/Prudect.js
@@ -24,6 +24,7 @@ const Prudectschema = new mongoose.Schema({
     },
     user: {
         type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
         required: true
     },
     username: {
